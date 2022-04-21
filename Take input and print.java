61f42899e5ed467de53d7410c79830465651c0fe
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2022-04-21 20:38:28 +0530

    Create Take input and print.java

---
+++ Take input and print.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Sample{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        String S;
+        S=s.nextLine();
+        System.out.println(S);
+    }
+}
