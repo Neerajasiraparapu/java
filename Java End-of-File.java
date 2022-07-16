f2f55180365865f9be36142fefa71be8398c46bb
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2022-07-16 10:07:15 +0530

    Create Java End-of-File.java

---
+++ Java End-of-File.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s=new Scanner(System.in);
+        int i=0;
+        while(s.hasNext()){
+             i++;
+             System.out.println(i+" "+s.nextLine());
+        }
+        
+    }
+}
